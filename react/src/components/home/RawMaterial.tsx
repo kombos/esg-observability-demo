b37c31818922ac900d48b67ae0670741c01aac83
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-09-19 14:58:37 +0600

    🏷️💅📕🛠️process page raw material tab added

---
+++ react/src/components/home/RawMaterial.tsx
@@ -0,0 +1,187 @@
+import React from "react";
+import { BsFillPuzzleFill } from "react-icons/bs";
+
+export default function RawMaterial() {
+  const DATA = {
+    title: "Inventory Assessment",
+    titleChip: (
+      <>
+        <BsFillPuzzleFill /> Product/Service - Shirt, Medium Size
+      </>
+    ),
+    processTitle: "Process",
+    processValue: "1b32feca67 - Cotton Fibre, organic ginning mass -0.9kg",
+    metaData: {
+      title: "Process Metadata",
+      objects: [
+        {
+          title: "Product Category",
+          value: "Textile",
+        },
+        {
+          title: "Process Type",
+          value: "LCI Result",
+        },
+        {
+          title: "Reference Year",
+          value: "2019",
+        },
+        {
+          title: "Compliance",
+          value: "EF 3.0",
+        },
+        {
+          title: "Location",
+          value: "UK+EU",
+        },
+      ],
+    },
+    resources: {
+      title: "Resources",
+      tableKeys: ["Resources", "Type", "Unit", "Amount", "Flow UUID", "Flow", "Method LCIA", "Characterization Factor"],
+      objects: [
+        {
+          Resources: "Cotton",
+          Type: "Input",
+          Unit: "kg",
+          Amount: "234",
+          "Flow UUID": "1181f5...43a5",
+          Flow: "(3-(sec...benzene",
+          "Method LCIA": "Warming...(GWP)",
+          "Characterization Factor": "kg CO₂-equivalents per kWh",
+        },
+        {
+          Resources: "Silk",
+          Type: "Input",
+          Unit: "kg",
+          Amount: "35",
+          "Flow UUID": "1181f5...43a5",
+          Flow: "graining silk",
+          "Method LCIA": "Warming...(GWP)",
+          "Characterization Factor": "kg CO₂-equivalents per kWh",
+        },
+        {
+          Resources: "Refined Cotton",
+          Type: "Output",
+          Unit: "kg",
+          Amount: "220",
+          "Flow UUID": "1181f5...43a5",
+          Flow: "cotton...ating",
+          "Method LCIA": "Eutrophication",
+          "Characterization Factor": "AQUATIC_EUTROPHICATION",
+        },
+        {
+          Resources: "Crude Oil",
+          Type: "Output",
+          Unit: "Litre",
+          Amount: "45",
+          "Flow UUID": "1181f5...43a5",
+          Flow: "cotton...cessing",
+          "Method LCIA": "Warming...(GWP)",
+          "Characterization Factor": "kg CO₂-equivalents per kWh",
+        },
+      ],
+    },
+    emissions: {
+      title: "Emissions",
+      tableKeys: ["LCIAMethod_uuid EF3.1", "LCIAMethod_name", "LCIAMethod_type", "LCIAMethod_impactIndicator"],
+      objects: [
+        {
+          "LCIAMethod_uuid EF3.1": "1181f5...43a5",
+          LCIAMethod_name: "Climate change-Fossil",
+          LCIAMethod_type: "MID_POINT_INDICATOR",
+          LCIAMethod_impactIndicator: "Radiative forcing as Global Warming Potential (GWP100)",
+        },
+        {
+          "LCIAMethod_uuid EF3.1": "1181f5...43a5",
+          LCIAMethod_name: "Ecotoxicity, freshwater",
+          LCIAMethod_type: "MID_POINT_INDICATOR",
+          LCIAMethod_impactIndicator: "Comparative Toxic Unit for ecosystems (CTUe)",
+        },
+        {
+          "LCIAMethod_uuid EF3.1": "1181f5...43a5",
+          LCIAMethod_name: "EF-particulate matter",
+          LCIAMethod_type: "MID_POINT_INDICATOR",
+          LCIAMethod_impactIndicator: "Impact on human health",
+        },
+        {
+          "LCIAMethod_uuid EF3.1": "1181f5...43a5",
+          LCIAMethod_name: "Resource use, minerals and metals",
+          LCIAMethod_type: "MID_POINT_INDICATOR",
+          LCIAMethod_impactIndicator: "Abiotic resource depletion (ADP ultimate reserve)",
+        },
+      ],
+    },
+    co_product: {
+      title: "Co Products",
+      products: ["Cotton Threads", "Silk Threads", "Cotton Dye"],
+    },
+  };
+
+  return (
+    <div className="process-form">
+      <div className="title">
+        <h1>{DATA?.title}</h1>
+        <div className="chip">{DATA?.titleChip}</div>
+      </div>
+      <div className="line"></div>
+
+      <div className="name">{DATA?.processTitle}</div>
+      <div className="value">{DATA?.processValue}</div>
+
+      <div className="name-2">{DATA?.metaData?.title}</div>
+      <div className="metadata-container">
+        {DATA?.metaData?.objects &&
+          Array.isArray(DATA?.metaData?.objects) &&
+          DATA?.metaData?.objects?.length > 0 &&
+          React.Children.toArray(
+            DATA?.metaData?.objects?.map((obj) => (
+              <div className="metadata">
+                <h3>{obj?.title}</h3>
+                <p>{obj?.value}</p>
+              </div>
+            )),
+          )}
+      </div>
+
+      <div className="name-3">{DATA?.resources?.title}</div>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>{React.Children.toArray(DATA?.resources?.tableKeys?.map((obj) => <th>{obj}</th>))}</tr>
+          </thead>
+          <tbody>
+            {React.Children.toArray(
+              DATA?.resources?.objects?.map((object) => (
+                <tr>{React.Children.toArray(DATA?.resources?.tableKeys?.map((obj) => <td>{object[obj]}</td>))}</tr>
+              )),
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="name-3">{DATA?.emissions?.title}</div>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>{React.Children.toArray(DATA?.emissions?.tableKeys?.map((obj) => <th>{obj}</th>))}</tr>
+          </thead>
+          <tbody>
+            {React.Children.toArray(
+              DATA?.emissions?.objects?.map((object) => (
+                <tr>{React.Children.toArray(DATA?.emissions?.tableKeys?.map((obj) => <td>{object[obj]}</td>))}</tr>
+              )),
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="name-3">{DATA?.co_product?.title}</div>
+      <ul>{React.Children.toArray(DATA?.co_product?.products?.map((li) => <li>{li}</li>))}</ul>
+
+      <div className="buttons">
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
