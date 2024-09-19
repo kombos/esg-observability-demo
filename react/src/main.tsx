61b34815c1633766efe42a0796e4fb85534a1e52
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-09-19 14:56:09 +0600

    💅updated

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import "./index.css";
+import "./styles/index.scss";
 import "@ignt/react-library/dist/style.css";
 import AddressProvider from "./def-hooks/addressContext";
 
